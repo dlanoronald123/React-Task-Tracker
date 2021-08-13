754fd39d2a3e314f992db60044a71b28fd720dac
Ronald Magno <dlanoronald123@gmail.com>
2021-08-13 21:48:07 +0800

    Addded tasks components for modifying tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import Header from './components/Header'
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "doc",
+      day: "ngayon",
+      reminder: true,
+    },
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks available"
+      )}
     </div>
   );
-}
+};
 
 export default App;
